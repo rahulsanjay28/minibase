104b37b603670347ca4b603e33e29136bf4974c5
Rahul Sanjay Avvaru <rahulsanjay@Rahuls-MacBook-Pro.local>
2020-02-21 00:01:45 -0700

    Indenting code to make it readable

--- bigt/BigT.java
+++ bigt/BigT.java
@@ -4,37 +4,43 @@ import global.MID;
 
 
 public class BigT {
-	
-	private String name;
-	private int type;
 
-// Defining different cluster strategy type
-	public BigT(String name, int type) {
-	
-	
-	}
-	// Delete the bigtable from the database
-	public void deleteBigt() {
-		
-	}
-	// Return number of maps in the bigtable
-	public int getMapCnt() {
-		return 0;
-	}
-	// Return number of distinct row labels in the bigtable
-	public int getRowCnt() {
-		return 0;
-	}
-	// Return number of distinct column labels in the bigtable
-	public int getColumnCnt() {
-		return 0;
-	}
-	// Insert map into the big table, return its Mid
-	public MID insertMap(byte[] mapPtr) {
-		return null;
-	}
-	// Initialize a stream of maps
-	public Stream openStream(int orderType, String rowFilter, String columnFilter, String valueFilter) {
-		return null;
-	}
+    private String name;
+    private int type;
+
+    // Defining different cluster strategy type
+    public BigT(String name, int type) {
+
+
+    }
+
+    // Delete the bigtable from the database
+    public void deleteBigt() {
+
+    }
+
+    // Return number of maps in the bigtable
+    public int getMapCnt() {
+        return 0;
+    }
+
+    // Return number of distinct row labels in the bigtable
+    public int getRowCnt() {
+        return 0;
+    }
+
+    // Return number of distinct column labels in the bigtable
+    public int getColumnCnt() {
+        return 0;
+    }
+
+    // Insert map into the big table, return its Mid
+    public MID insertMap(byte[] mapPtr) {
+        return null;
+    }
+
+    // Initialize a stream of maps
+    public Stream openStream(int orderType, String rowFilter, String columnFilter, String valueFilter) {
+        return null;
+    }
 }
--- bigt/Stream.java
+++ bigt/Stream.java
@@ -4,14 +4,14 @@ import global.MID;
 
 public class Stream {
 
-	public Stream(BigT bigtable, int orderType, String rowFilter, String columnFilter, String valueFilter) {
-	}
-	
-	public void closeStream() {
-		
-	}
-	
-	public Map getNext(MID mid) {
-		return null;
-	}
+    public Stream(BigT bigtable, int orderType, String rowFilter, String columnFilter, String valueFilter) {
+    }
+
+    public void closeStream() {
+
+    }
+
+    public Map getNext(MID mid) {
+        return null;
+    }
 }
