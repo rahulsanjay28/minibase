0774a4676774963c6418cd6003910af6487bd0a9
Rahul Sanjay Avvaru <rahulsanjay@Rahuls-MacBook-Pro.local>
2020-03-09 13:28:39 -0700

    adding provision to update the buffer pool size in buffermanager

--- bufmgr/BufMgr.java
+++ bufmgr/BufMgr.java
@@ -2,11 +2,15 @@
 
 package bufmgr;
 
-import java.io.*;
-import java.util.*;
+import diskmgr.DiskMgrException;
+import diskmgr.FileIOException;
+import diskmgr.InvalidPageNumberException;
+import diskmgr.Page;
+import global.GlobalConst;
+import global.PageId;
+import global.SystemDefs;
 
-import diskmgr.*;
-import global.*;
+import java.io.IOException;
 
 
 /**
@@ -284,7 +288,7 @@ class Clock extends Replacer {
      *
      * @return -1 if no frame is available.
      * head of the list otherwise.
-     * @throws BufferPoolExceededException.
+     * @throws BufferPoolExceededException
      */
     public int pick_victim()
             throws BufferPoolExceededException,
@@ -499,7 +503,7 @@ public class BufMgr implements GlobalConst {
      * if emptyPage==TRUE, then actually no read is done to bring
      * the page in.
      *
-     * @param Page_Id_in_a_DB page number in the minibase.
+     * @param pin_pgid page number in the minibase.
      * @param page            the pointer poit to the page.
      * @param emptyPage       true (empty page); false (non-empty page)
      * @throws ReplacerException           if there is a replacer error.
@@ -611,7 +615,7 @@ public class BufMgr implements GlobalConst {
      * put it in a group of replacement candidates.
      * if pincount=0 before this call, return error.
      *
-     * @param globalPageId_in_a_DB page number in the minibase.
+     * @param PageId_in_a_DB page number in the minibase.
      * @param dirty                the dirty bit of the frame
      * @throws ReplacerException           if there is a replacer error.
      * @throws PageUnpinnedException       if there is a page that is already unpinned.
@@ -829,6 +833,9 @@ public class BufMgr implements GlobalConst {
         return numBuffers;
     }
 
+    public void setNumBuffers(int numBuf){
+        this.numBuffers = numBuf;
+    }
 
     /**
      * Gets the total number of unpinned buffer frames.
