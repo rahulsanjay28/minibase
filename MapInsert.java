8cb1dd89f458b256189e241fb5c9adb1ea1b191d
rangheet <heetdave@outlook.com>
2020-04-10 18:19:41 -0700

    Added setNumBuffers(0) to persist data

--- MapInsert.java
+++ MapInsert.java
@@ -1,6 +1,9 @@
+import bigt.BigTStream;
 import bigt.Map;
 import bigt.Minibase;
 import Utility.GetMap;
+import global.MID;
+import global.SystemDefs;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -17,5 +20,21 @@ public class MapInsert {
         List<Map> list = new ArrayList<>();
         list.add(GetMap.getMap(rowLabel, columnLabel, value, timeStamp));
         Minibase.getInstance().getBigTable().insertSingleMap(list, Integer.parseInt(type));
+
+//        BigTStream stream=null;
+//        for(int i = 1; i<=5; i++){
+//            System.out.println("PRITING FOR TYPE------ " + i);
+//            stream = Minibase.getInstance().getBigTable().getBigTableParts().get(i).openStream("*","*","*");
+//            MID mid = new MID();
+//            Map map = stream.getNext(mid);
+//            while(map!=null){
+//                map.print();
+//                mid = new MID();
+//                map = stream.getNext(mid);
+//            }
+//            stream.closeStream();
+//        }
+
+        SystemDefs.JavabaseBM.setNumBuffers(0);
     }
 }
